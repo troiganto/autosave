9bbf3b222c654b927a396b3af7680670dc629d75
troiganto <uebertreiber@gmx.de>
2014-07-10 23:27:17 +0200

    Refactored DocumentIconFinderTests.
    The file paths the tests rely on now are at the beginning of the file,
    for easier reference.
    A test for exe files has been added.

--- AutoSave_tests/DocumentIconFinderTests.cpp
+++ AutoSave_tests/DocumentIconFinderTests.cpp
@@ -9,11 +9,22 @@ namespace AutoSave_tests
 	TEST_CLASS(DocumentIconFinderTests)
 	{
 	public:
-		
+
+		wstring imageres = LR"(C:\Windows\system32\imageres.dll)";
+		wstring pngapp = LR"(C:\Program Files (x86)\IrfanView\i_view32.exe)";
+		wstring icofile = LR"(C:\dev\autosave\AutoSave Icons\main.ico)";
+		wstring saiapp = LR"(C:\Users\Nico\Downloads\PaintToolSAI\sai.exe)";
+		wstring oggapp = LR"(C:\Program Files (x86)\VideoLAN\VLC\vlc.exe)";
+#ifndef _WIN64
+		wstring self = LR"(C:\PROGRAM FILES (X86)\MICROSOFT VISUAL STUDIO 12.0\COMMON7\IDE\COMMONEXTENSIONS\MICROSOFT\TESTWINDOW\vstest.executionengine.x86.exe)";
+#else
+		wstring self = LR"(C:\PROGRAM FILES (X86)\MICROSOFT VISUAL STUDIO 12.0\COMMON7\IDE\COMMONEXTENSIONS\MICROSOFT\TESTWINDOW\vstest.executionengine.exe)";
+#endif
+
 		TEST_METHOD(TestDIFTextDocuments)
 		{
 			DocumentIconFinder dif(L"test.txt");
-			Assert::AreEqual<wstring>(LR"(C:\Windows\system32\imageres.dll)",
+			Assert::AreEqual<wstring>(imageres,
 				dif.iconFile, L"didn't find TXT icon file");
 			Assert::AreEqual(-102, dif.iconIndex, L"didn't find TXT icon index");
 		}
@@ -23,16 +34,14 @@ namespace AutoSave_tests
 			DocumentIconFinder dif(L"test.png");
 			// To be replaced with yout own picture viewer's icon.
 			Assert::AreEqual<wstring>(
-				LR"(C:\Program Files (x86)\IrfanView\i_view32.exe)",
-				dif.iconFile, L"didn't find PNG icon file");
+				pngapp, dif.iconFile, L"didn't find PNG icon file");
 			Assert::AreEqual(0, dif.iconIndex, L"didn't find PNG icon index");
 		}
 
 		TEST_METHOD(TestDIFIconFiles)
 		{
-			wstring filePath = LR"(C:\dev\autosave\AutoSave Icons\main.ico)";
-			DocumentIconFinder dif(filePath);
-			Assert::AreEqual<wstring>(filePath, dif.iconFile,
+			DocumentIconFinder dif(icofile);
+			Assert::AreEqual<wstring>(icofile, dif.iconFile,
 				L"didn't find ICO icon file (itself)");
 			Assert::AreEqual(0, dif.iconIndex, L"didn't find icon file index (0)");
 		}
@@ -47,23 +56,26 @@ namespace AutoSave_tests
 			Assert::AreEqual(1, dif.iconIndex, L"didn't find SAI icon index");
 		}
 
+		TEST_METHOD(TestDIFExeFiles)
+		{
+			// Tests the unusual .sai\DefaultIcon structure.
+			DocumentIconFinder dif(saiapp);
+			Assert::AreEqual<wstring>(
+				saiapp, dif.iconFile, L"didn't find EXE icon file (itself)");
+			Assert::AreEqual(0, dif.iconIndex, L"didn't find EXE icon index (0)");
+		}
+
 		TEST_METHOD(TestDIFOggFiles)
 		{
 			// Tests quotes in the icon location string.
 			DocumentIconFinder dif(L"test.ogg");
 			Assert::AreEqual<wstring>(
-				LR"(C:\Program Files (x86)\VideoLAN\VLC\vlc.exe)",
-				dif.iconFile, L"didn't find OGG icon file");
+				oggapp, dif.iconFile, L"didn't find OGG icon file");
 			Assert::AreEqual(0, dif.iconIndex, L"didn't find OGG icon index");
 		}
 
 		TEST_METHOD(TestDIFDefaultIcon)
 		{
-#ifndef _WIN64
-			wstring self = LR"(C:\PROGRAM FILES (X86)\MICROSOFT VISUAL STUDIO 12.0\COMMON7\IDE\COMMONEXTENSIONS\MICROSOFT\TESTWINDOW\vstest.executionengine.x86.exe)";
-#else
-			wstring self = LR"(C:\PROGRAM FILES (X86)\MICROSOFT VISUAL STUDIO 12.0\COMMON7\IDE\COMMONEXTENSIONS\MICROSOFT\TESTWINDOW\vstest.executionengine.exe)";
-#endif
 			DocumentIconFinder dif(L"blah");
 			Assert::AreEqual<wstring>(self, dif.iconFile,
 				L"didn't find default icon file");
