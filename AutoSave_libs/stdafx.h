a5e463b67d0626a75b8510368c65d7045dc21d05
troiganto <uebertreiber@gmx.de>
2014-07-11 18:12:20 +0200

    UI fix and bump to v0.9.6.
    Removed the addition "advanced" from "This is a regular expression".

--- AutoSave_libs/stdafx.h
+++ AutoSave_libs/stdafx.h
@@ -12,7 +12,7 @@
 #define DEFAULT_REGISTRY_KEY APP_NAME
 #define CONNECTED_SHORTCUTS_REGISTRY_VALUE_NAME L"connectedShortcutsList"
 #define SHORT_APP_NAME L"AutoSave"
-#define APP_VERSION2 L"0.9.5"
+#define APP_VERSION2 L"0.9.6"
 #ifdef _WIN64
 #define APP_VERSION APP_VERSION2
 #else
